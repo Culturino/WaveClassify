a3a2e594a27e28cb82126e53f9d477781b53a03f
Culturino <m.sattorov2005@gmail.com>
2025-06-05 09:15:00 +0200

    implement tf dataset padding and batching

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import numpy as np
 from config import DATA_DIR
+import tensorflow as tf
 
 def load_split(split_name):
     path = f"{DATA_DIR}/{split_name}.npz"
@@ -9,3 +10,22 @@ def load_split(split_name):
     except:
         print("no file")
         return [np.random.rand(50, 3) for _ in range(100)], np.random.randint(0, 2, (100, 5)).astype(np.float32)
+
+
+
+
+def dataset_maker(x, y, n_features, n_classes, batch_size, shuffle=False):
+    ds = tf.data.Dataset.from_generator(
+        lambda: zip(x, y),
+        output_signature=(
+            tf.TensorSpec(shape=(None, n_features), dtype=tf.float32),
+            tf.TensorSpec(shape=(n_classes,), dtype=tf.float32)
+        )
+    )
+    if shuffle: ds = ds.shuffle(2048)
+    ds = ds.padded_batch(
+        batch_size,
+        padded_shapes=([None, n_features], [n_classes]),
+        padding_values=(0.0, 0.0)
+    )
+    return ds.prefetch(tf.data.AUTOTUNE)
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from config import *
-from data import load_split
+from data import load_split, dataset_maker
 
 def main():
     print("start")
     x_train, y_train = load_split('train')
-    x_val, y_val = load_split('val')
-    x_test, y_test = load_split('test')
-    print("done")
+    try:
+        n_feat, n_class = x_train[0].shape[1], y_train.shape[1]
+        ds = dataset_maker(x_train, y_train, n_feat, n_class, BATCH_SIZE)
+        print("done")
+    except:
+        print("oops")
 
 if __name__ == "__main__":
     main()
