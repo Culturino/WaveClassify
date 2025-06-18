ece872c883f7199fb544c730d7004a2e967bffdc
Culturino <m.sattorov2005@gmail.com>
2025-06-18 09:10:00 +0200

    add early stopping callback

--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 import numpy as np
+import tensorflow as tf
 
 def train_model(model, train_ds, val_ds, n_train, batch_size, epochs):
     train_ds_rep = train_ds.repeat()
     steps_per_epoch = int(np.ceil(n_train / batch_size))
+    cb = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
     
     hist = model.fit(
         train_ds_rep, validation_data=val_ds, epochs=epochs,
-        steps_per_epoch=steps_per_epoch, verbose=1
+        steps_per_epoch=steps_per_epoch, callbacks=[cb], verbose=1
     )
     return hist
